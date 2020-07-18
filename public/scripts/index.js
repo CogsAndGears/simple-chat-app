c526bfb847c1026079d2785564e495d878380533
cogs <cogs@springless.com>
2020-07-18 14:48:39 -0700

    Cleanup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -139,26 +139,7 @@ async function getMedia() {
         console.warn("Could not find local video div");
     }
     
-    //stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
 }
 
 getMedia();
-
-/*
-navigator.mediaDevices.getUserMedia(
-  { video: true, audio: true },
-  stream => {
-    console.warn("Got here");
-    const localVideo = document.getElementById("local-video");
-    if (localVideo) {
-      localVideo.srcObject = stream;
-    }
-    console.warn("Got here");
-
-    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-  },
-  error => {
-    alert("Problem");
-    console.warn(error.message);
-  }
-);*/
